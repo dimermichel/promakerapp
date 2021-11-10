dc2403687a269974577f3e589fc450f9b262f28d
Michel Maia <dimermichel@gmail.com>
2021-11-10 01:05:46 -0300

    Add Edit

--- src/components/ScriptCard/index.tsx
+++ src/components/ScriptCard/index.tsx
@@ -1,5 +1,11 @@
+import React, {
+ useCallback, useRef, useMemo, useState 
+} from 'react';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
-import React from 'react';
+import { BottomSheetBackdrop, BottomSheetModal } from '@gorhom/bottom-sheet';
+import { useTheme } from 'styled-components';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {
   Container,
   Title,
@@ -9,9 +15,18 @@ import {
   Icon,
   Date,
   IconButton,
+  BottomSheetContainer,
+  BottomSheetContent,
+  BottomSheetActionsContainer,
+  ActionsTextButton,
+  ActionsText,
+  SurroundingButton,
+  ContainerButton,
 } from './styles';
+import { useAuth } from '../../hooks/auth';
 
 export interface ScriptCardProps {
+  id: string;
   title: string;
   text: string;
   date: string;
@@ -22,22 +37,139 @@ interface Props {
 }
 
 export default function ScriptCard({ data }: Props) {
+  const [isScriptDeleted, setIsScriptDeleted] = useState(false);
   const navigation = useNavigation();
-  return (
-    <Container>
-      <Title>{data.title}</Title>
-      <Text ellipsizeMode="tail" numberOfLines={3} style={{ width: 100 }}>
-        {data.text}
-      </Text>
+  const theme = useTheme();
+  const { user } = useAuth();
 
-      <Footer>
-        <IconButton onPress={() => navigation.navigate('CameraRecord', data)}>
-          <Icon name="play" />
-        </IconButton>
-        <IconContainer>
-          <Date>{data.date}</Date>
-        </IconContainer>
-      </Footer>
-    </Container>
+  const dataKey = `@promakerapp:script_user:${user.id}`;
+  const backdropPressBehavior = 'close';
+  // hooks
+  const bottomSheetActionsModalRef = useRef<BottomSheetModal>(null);
+  // variables
+  const snapPoints = useMemo(() => ['12%', '25%'], []);
+
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetActionsModalRef.current?.present();
+  }, []);
+
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
+  const renderBackdrop = useCallback(
+    (props) => (
+      <BottomSheetBackdrop {...props} pressBehavior={backdropPressBehavior} />
+    ),
+    [backdropPressBehavior],
+  );
+  const handleDelete = useCallback(async () => {
+    const { id } = data;
+
+    if (id) {
+      try {
+        // TODO Delete Script
+        const response = await AsyncStorage.getItem(dataKey);
+        const speechesSaved = response ? JSON.parse(response) : [];
+        const newSpeechesSaved = speechesSaved.filter(
+          (speech) => speech.id !== id,
+        );
+        await AsyncStorage.setItem(dataKey, JSON.stringify(newSpeechesSaved));
+        setIsScriptDeleted(true);
+        bottomSheetActionsModalRef.current?.close();
+      } catch (err) {
+        Alert.alert('error', err.message);
+      }
+    }
+  }, [data.id]);
+  const handleEdit = () => {
+    bottomSheetActionsModalRef.current?.close();
+    navigation.push('Speech', data.id);
+  };
+
+  const handleDeleteModal = useCallback(() => {
+    Alert.alert('Delete Script', 'Would you like to delete the script?', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'Delete', onPress: () => handleDelete() },
+    ]);
+  }, [handleDelete]);
+  return (
+    <>
+      <>
+        {!isScriptDeleted ? (
+          <ContainerButton onPress={handlePresentModalPress}>
+            <Title>{data.title}</Title>
+            <Text ellipsizeMode="tail" numberOfLines={3} style={{ width: 100 }}>
+              {data.text}
+            </Text>
+
+            <Footer>
+              <IconButton
+                onPress={() => navigation.navigate('CameraRecord', data)}
+              >
+                <Icon name="play" />
+              </IconButton>
+              {/* <IconButton onPress={handlePresentModalPress}>
+                <Icon name="more-vertical" />
+              </IconButton> */}
+              <IconContainer>
+                <Date>{data.date}</Date>
+              </IconContainer>
+            </Footer>
+          </ContainerButton>
+        ) : (
+          <Container>
+            <Title>This script has been deleted...</Title>
+          </Container>
+        )}
+      </>
+      <BottomSheetModal
+        ref={bottomSheetActionsModalRef}
+        backdropComponent={renderBackdrop}
+        index={1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+      >
+        <BottomSheetContainer>
+          <BottomSheetContent>
+            <Text
+              style={{
+                marginBottom: 16,
+                fontFamily: theme.fonts.medium,
+                fontSize: 18,
+                color: '#464E5F',
+              }}
+            >
+              Actions
+            </Text>
+            <BottomSheetActionsContainer>
+              <ActionsTextButton onPress={handleEdit}>
+                <Icon
+                  name="edit"
+                  size={14}
+                  color="#464e5f"
+                  style={{ marginRight: 8 }}
+                />
+                <ActionsText>Edit</ActionsText>
+              </ActionsTextButton>
+              <ActionsTextButton onPress={handleDeleteModal}>
+                <Icon
+                  name="x-square"
+                  size={14}
+                  color="#464e5f"
+                  style={{ marginRight: 8 }}
+                />
+                <ActionsText>Delete</ActionsText>
+              </ActionsTextButton>
+            </BottomSheetActionsContainer>
+          </BottomSheetContent>
+        </BottomSheetContainer>
+      </BottomSheetModal>
+    </>
   );
 }
--- src/components/ScriptCard/styles.ts
+++ src/components/ScriptCard/styles.ts
@@ -4,6 +4,14 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
+  background-color: ${({ theme }) => theme.colors.secondary_light};
+  border-radius: 5px;
+
+  padding: 17px 24px;
+  margin-bottom: 16px;
+`;
+
+export const ContainerButton = styled(RectButton)`
   background-color: ${({ theme }) => theme.colors.shape};
   border-radius: 5px;
 
@@ -35,7 +43,7 @@ export const IconContainer = styled.View`
 
 export const Icon = styled(Feather)`
   font-size: ${RFValue(20)}px;
-  color: ${({ theme }) => theme.colors.text};
+  color: ${({ theme }) => theme.colors.secondary_light};
 `;
 
 export const IconButton = styled(RectButton)`
@@ -43,8 +51,58 @@ export const IconButton = styled(RectButton)`
   color: ${({ theme }) => theme.colors.text};
 `;
 
+export const SurroundingButton = styled(RectButton)`
+  flex: 1;
+`;
+
 export const Date = styled.Text`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(14)}px;
   color: ${({ theme }) => theme.colors.text};
+`;
+
+export const BottomSheetContainer = styled.View`
+  flex: 1;
+  padding: 24px;
+  background-color: #fff;
+`;
+
+export const BottomSheetContent = styled.View`
+  flex: 1;
+  align-items: center;
+`;
+
+export const BottomSheetActionsContainer = styled.View`
+  flex: 1;
+  align-items: flex-start;
+  justify-content: space-around;
+  width: 90%;
+`;
+
+export const ActionsTextButton = styled.TouchableOpacity`
+  flex: 1;
+  flex-direction: row;
+  width: 100%;
+  padding: 0 10px;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const ActionsText = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(16)}px;
+
+  color: #464e5f;
+`;
+
+export const DeletedPostTextContainer = styled.View`
+  flex-direction: column;
+  padding-bottom: 16px;
+  padding-top: 4px;
+`;
+
+export const DeletedPostText = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+  color: #b5b5c3;
 `;
